e844c75bc22eedc55621645d58546f20babd1613
oudra1729 <brahimbra12356@gmail.com>
2025-08-11 18:38:53 +0100

    secound commit

---
+++ week1/day1/exercices.py
@@ -0,0 +1,32 @@
+# ex1:Hello World
+# print("Hello, World!\n" * 4)
+
+
+# ex2 :Some Math
+# result = (99 ** 3) * 8
+# print(result)
+
+# ex3 :What’s your name ?
+# user_name = input("What is your name? ")
+# if user_name == "dark":
+#     print("Hey, we have the same name!")
+# else:
+#     print("Nice to meet you, " + user_name + "!")
+
+
+
+#  ex4 :Tall enough to ride a roller coaster
+# user_height  = int(input("What is your height ? "))
+# if user_height > 145:
+#     print("You are tall enough to ride.") 
+# else:
+#     print("You need to grow some more to ride.")
+
+
+#Exe 5 : Favorite Numbers
+my_fav_numbers = {7, 42, 99}
+my_fav_numbers.add(13)
+my_fav_numbers.remove(99)
+my_fav_numbers.pop()
+friend_fav_numbers = {42, 88, 13}
+our_fav_numbers = my_fav_numbers.union(friend_fav_numbers)
