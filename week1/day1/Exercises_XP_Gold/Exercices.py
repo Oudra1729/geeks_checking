c7bfe6872b717d4e72ba319f862732ab417ba49b
oudra1729 <brahimbra12356@gmail.com>
2025-08-12 09:56:44 +0100

    exercies of gold

---
+++ week1/day1/Exercises_XP_Gold/Exercices.py
@@ -0,0 +1,35 @@
+# ex1
+# month = 0
+# while month < 1 or month > 12:
+#     month = int(input("Enter a month (1-12): "))
+#     if 3 <= month <= 5:
+#         print("It's spring!")
+
+#     elif 6 <= month <= 8:
+#         print("It's summer!")
+
+#     elif 9 <= month <= 11:
+#         print("It's Autumn!")
+
+#     else:
+#         print("It's Winter!")
+
+
+# ex2
+# for i in range(1, 21):
+#     print(i)
+    
+# ex3
+# for i in range(1, 21):
+#     if i % 2 == 0:
+#         print(i)
+
+
+# ex4
+# name = "dark"
+# while name != "dark":
+#     name = input("Enter your name: ")
+#     if name == "dark":
+#         print("Welcome back, dark!")
+
+
