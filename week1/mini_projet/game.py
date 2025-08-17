23f59dceabeaec70568203e8e3486fd2d1551068
oudra1729 <brahimbra12356@gmail.com>
2025-08-17 02:00:57 +0100

    feat: implement get_game_result logic

--- week1/mini_projet/game.py
+++ week1/mini_projet/game.py
@@ -15,7 +15,14 @@ class Game:
         return random.choice(["rock", "paper", "scissors"])
 
     def get_game_result(self, user_item, computer_item):
-        pass
+        if user_item == computer_item:
+            return "draw"
+        elif (user_item == "rock" and computer_item == "scissors") or \
+             (user_item == "scissors" and computer_item == "paper") or \
+             (user_item == "paper" and computer_item == "rock"):
+            return "win"
+        else:
+            return "loss"
 
     def play(self):
         pass
