5e03ac1b4616cac351bb0d1614f08abf815fd6c5
oudra1729 <brahimbra12356@gmail.com>
2025-08-17 02:03:54 +0100

    feat: implement get_user_menu_choice with validation

--- week1/mini_projet/rock-paper-scissors.py
+++ week1/mini_projet/rock-paper-scissors.py
@@ -2,8 +2,17 @@
 from game import Game
 
 def get_user_menu_choice():
-    pass
+    print("\n Menu:")
+    print("(g) Play a new game")
+    print("(s) Show scores")
+    print("(q) Quit")
 
+    choice = input("Enter your choice: ").lower()
+    if choice in ["g", "s", "q"]:
+        return choice
+    else:
+        print("Invalid choice. Try again.")
+        return None
 def print_results(results):
     pass
 
