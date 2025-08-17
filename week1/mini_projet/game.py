ef7d7038db3b5cfc7d59344e7c5ebe3d48f0b0f9
oudra1729 <brahimbra12356@gmail.com>
2025-08-17 01:59:38 +0100

    feat: implement get_user_item with validation

--- week1/mini_projet/game.py
+++ week1/mini_projet/game.py
@@ -3,7 +3,13 @@ import random
 
 class Game:
     def get_user_item(self):
-        pass
+        choices = ["rock", "paper", "scissors"]
+        while True:
+            user_choice = input("Select (rock/paper/scissors): ").lower()
+            if user_choice in random.choices:
+                return user_choice
+            else:
+                print(" Invalid choice, try again...")
 
     def get_computer_item(self):
         pass
