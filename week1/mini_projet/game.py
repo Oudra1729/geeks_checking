894dd7357236d5deb02f61bda994938c0b75f9f3
oudra1729 <brahimbra12356@gmail.com>
2025-08-17 01:58:06 +0100

    feat: add Game class skeleton in game.py

---
+++ week1/mini_projet/game.py
@@ -0,0 +1,15 @@
+
+import random
+
+class Game:
+    def get_user_item(self):
+        pass
+
+    def get_computer_item(self):
+        pass
+
+    def get_game_result(self, user_item, computer_item):
+        pass
+
+    def play(self):
+        pass
