ea0088e991f16482d1ea58d940155944c84e3265
oudra1729 <brahimbra12356@gmail.com>
2025-08-25 11:24:21 +0100

    fixed problem day2/week1

--- week1/day2/Challenge/Challenge.py
+++ week1/day2/Challenge/Challenge.py
@@ -1,14 +1,10 @@
 
-# word = input("Enter a word: ")
-# letter_indexes = {}
-# for index, letter in enumerate(word):
-#     if letter not in letter_indexes:
-#         letter_indexes[letter] = []
-#     letter_indexes[letter].append(index)
-# print(letter_indexes)
+word = input("Enter a word: ")
+letter_indexes = {}
+for index, letter in enumerate(word):
+    if letter not in letter_indexes:
+        letter_indexes[letter] = []
+    letter_indexes[letter].append(index)
+print(letter_indexes)
 
-for letter in 'oudraibrq':
-    if letter == 'a':
-        continue
-    print(letter, end='-')
 
