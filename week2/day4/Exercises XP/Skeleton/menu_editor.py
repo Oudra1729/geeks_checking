6907c05fff381402f0d49130cff5015ca5fa3b1b
oudra1729 <brahimbra12356@gmail.com>
2025-08-26 11:07:08 +0100

    add show_user_menu function with user input handling

---
+++ week2/day4/Exercises XP/Skeleton/menu_editor.py
@@ -0,0 +1,31 @@
+
+from menu_item import MenuItem
+from menu_manager import MenuManager
+
+def show_user_menu():
+    while True:
+        print("\n====== Program Menu ======")
+        print("V - View an Item")
+        print("A - Add an Item")
+        print("D - Delete an Item")
+        print("U - Update an Item")
+        print("S - Show the Menu")
+        print("X - Exit")
+        choice = input("Please choose an option: ").upper()
+
+        if choice == "V":
+            view_item()
+        elif choice == "A":
+            add_item_to_menu()
+        elif choice == "D":
+            remove_item_from_menu()
+        elif choice == "U":
+            update_item_from_menu()
+        elif choice == "S":
+            show_restaurant_menu()
+        elif choice == "X":
+            print("\nExiting program... Here is the final menu:\n")
+            show_restaurant_menu()
+            break
+        else:
+            print("❌ Invalid choice, try again.")
