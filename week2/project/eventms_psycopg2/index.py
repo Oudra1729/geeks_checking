5ed01816ccd12f219dbeb77e3f882e44ea91cbec
oudra1729 <brahimbra12356@gmail.com>
2025-08-30 19:00:20 +0100

    update

--- week2/project/eventms_psycopg2/index.py
+++ week2/project/eventms_psycopg2/index.py
@@ -9,15 +9,21 @@ from attendees.routes import attendees_bp
 from tickets.routes import tickets_bp
 from stats_routes import stats_bp
 
-def create_app():
-    load_dotenv()
-    app = Flask(__name__, template_folder='templates', static_folder='static')
-    app.config['SECRET_KEY'] = os.getenv('FLASK_SECRET_KEY', 'dev-secret-change-me')
 
-    # init db pool
+load_dotenv()
+
+def create_app():
+    app = Flask(__name__, template_folder='templates', static_folder='static')
+
+    
+    app.config['SECRET_KEY'] = os.getenv("FLASK_SECRET_KEY")
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+  
     init_db_pool()
 
-    # register login
+    # login manager
     login_manager.init_app(app)
     ensure_admin_seed()  # makes sure default admin user exists with 'password'
 
